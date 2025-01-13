fd9a79855988c7d82880972638f1243dd031d3cd
amirhossein babaei <amirb@gmail.com>
2025-01-13 15:26:35 +0330

    add templates & add app home & migrate database & and other changes

--- Dental/Dental/settings/base.py
+++ Dental/Dental/settings/base.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -38,7 +39,7 @@ ROOT_URLCONF = 'Dental.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ Dental/home/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# if need to banner or images or another things we back
+
+
+# class Banner(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
+#     image = models.ImageField()
+
+#     def __str__(self):
+#         return self.title
